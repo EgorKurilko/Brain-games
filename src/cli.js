326b54998c189c02af84b88245ed4f8e4b62b319
Egor Kurilko <kypulko_egor@bk.ru>
2023-04-11 00:33:40 +0300

    Changed file

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('MayI have your name? ');
-  // const userName = readlineSync.keyInYNStrict('Your answer: ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
