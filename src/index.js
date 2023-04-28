920c2728104cf59e9148cab56c2147998627f0f4
Egor Kurilko <kypulko_egor@bk.ru>
2023-04-28 16:50:56 +0300

    Modified files

--- src/index.js
+++ src/index.js
@@ -2,25 +2,26 @@ import readlineSync from 'readline-sync';
 
 const runGame = (description, getQuestion, getRightAnswer) => {
   console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
+
   console.log(`Hello, ${userName}!`);
-  // Это описание специфичное для игры even (S)
   console.log(description);
 
-  // 3 раза задаём вопрос во вссех играх (A)
-  let sum = 0;
-  while (sum !== 3) {
-    const question = getQuestion(); // Сочиняем вопрос (S) num -- это вопрос
-    const rightAnswer = getRightAnswer(question); // Получаем правильный ответ (S)
-    console.log(`Question: ${question}`); // Задаём вопрос (A)
-    const userAnswer = readlineSync.question('Your answer: '); // Получаем ответ (A) (S) ???
-    if (String(rightAnswer) !== userAnswer) { // Сравниваем ответ поьзователя с правильным
+  let count = 0;
+  const maxNumOfRounds = 3;
+  while (count !== maxNumOfRounds) {
+    const question = getQuestion();
+    const rightAnswer = getRightAnswer(question);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (String(rightAnswer) !== userAnswer) {
       console.log(`'${userAnswer}' is wrong answer; Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
-    sum += 1;
+    count += 1;
   }
   console.log(`Congratulations, ${userName}!`);
 };
